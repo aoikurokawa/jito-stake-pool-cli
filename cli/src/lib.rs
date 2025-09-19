59c104dd9931fe3ef7400d954f6cde16a498ea81
aoikurokawa <ht.so.ik0102@gmail.com>
2025-09-19 12:19:57 +0900

    feat: update

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,4 +1,3 @@
-use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
     commitment_config::CommitmentConfig, instruction::Instruction, message::Message,
     signers::Signers, transaction::Transaction,
--- sdk/src/sdk/add_validator.rs
+++ sdk/src/sdk/add_validator.rs
@@ -5,65 +5,52 @@ use solana_sdk::{
     sysvar,
 };
 use spl_stake_pool::{
-    find_stake_program_address, find_transient_stake_program_address,
-    find_withdraw_authority_program_address, state::StakePool,
+    find_stake_program_address, find_withdraw_authority_program_address, state::StakePool,
 };
 
-pub fn increase_validator_stake_with_vote(
+pub fn add_validator_to_pool_with_vote(
     program_id: &Pubkey,
     stake_pool: &StakePool,
     stake_pool_address: &Pubkey,
+    funder: &Pubkey,
     vote_account_address: &Pubkey,
-    lamports: u64,
-    transient_stake_seed: u64,
 ) -> Instruction {
     let pool_withdraw_authority =
         find_withdraw_authority_program_address(program_id, stake_pool_address).0;
-    let (transient_stake_address, _) = find_transient_stake_program_address(
-        program_id,
-        vote_account_address,
-        stake_pool_address,
-        transient_stake_seed,
-    );
-    let (validator_stake_address, _) =
+    let (stake_account_address, _) =
         find_stake_program_address(program_id, vote_account_address, stake_pool_address, None);
-
-    increase_validator_stake(
+    add_validator_to_pool(
         program_id,
         stake_pool_address,
         &stake_pool.staker,
+        funder,
         &pool_withdraw_authority,
         &stake_pool.validator_list,
-        &stake_pool.reserve_stake,
-        &transient_stake_address,
-        &validator_stake_address,
-        lamports,
-        transient_stake_seed,
+        &stake_account_address,
+        vote_account_address,
     )
 }
 
-pub fn increase_validator_stake(
+pub fn add_validator_to_pool(
     program_id: &Pubkey,
     stake_pool: &Pubkey,
     staker: &Pubkey,
-    stake_pool_withdraw_authority: &Pubkey,
+    funder: &Pubkey,
+    stake_pool_withdraw: &Pubkey,
     validator_list: &Pubkey,
-    reserve_stake: &Pubkey,
-    transient_stake: &Pubkey,
+    stake: &Pubkey,
     validator: &Pubkey,
-    lamports: u64,
-    transient_stake_seed: u64,
 ) -> Instruction {
     let accounts = vec![
-        AccountMeta::new_readonly(*stake_pool, false),
+        AccountMeta::new(*stake_pool, false),
         AccountMeta::new_readonly(*staker, true),
-        AccountMeta::new_readonly(*stake_pool_withdraw_authority, false),
+        AccountMeta::new(*funder, true),
+        AccountMeta::new_readonly(*stake_pool_withdraw, false),
         AccountMeta::new(*validator_list, false),
-        AccountMeta::new(*reserve_stake, false),
-        AccountMeta::new(*transient_stake, false),
+        AccountMeta::new(*stake, false),
         AccountMeta::new_readonly(*validator, false),
-        AccountMeta::new_readonly(sysvar::clock::id(), false),
         AccountMeta::new_readonly(sysvar::rent::id(), false),
+        AccountMeta::new_readonly(sysvar::clock::id(), false),
         AccountMeta::new_readonly(sysvar::stake_history::id(), false),
         AccountMeta::new_readonly(solana_stake_interface::config::id(), false),
         AccountMeta::new_readonly(solana_system_interface::program::id(), false),
@@ -72,11 +59,8 @@ pub fn increase_validator_stake(
     Instruction {
         program_id: *program_id,
         accounts,
-        data: spl_stake_pool_legacy::instruction::StakePoolInstruction::IncreaseValidatorStake {
-            lamports,
-            transient_stake_seed,
-        }
-        .try_to_vec()
-        .unwrap(),
+        data: spl_stake_pool_legacy::instruction::StakePoolInstruction::AddValidatorToPool
+            .try_to_vec()
+            .unwrap(),
     }
 }
